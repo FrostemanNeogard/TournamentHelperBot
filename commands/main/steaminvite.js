d4fda862d852868d87163988b4969167839aab74
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-03 01:21:38 +0200

    change command option to accept steam profile url instead of invite url

--- commands/main/steaminvite.js
+++ commands/main/steaminvite.js
@@ -6,7 +6,12 @@ const {
   ActionRowBuilder,
 } = require("discord.js");
 
+const axios = require("axios");
+
 const { main_color } = require("../../config.json");
+
+require("dotenv").config();
+const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 module.exports = {
   name: "steaminvite",
@@ -15,22 +20,56 @@ module.exports = {
     .setDescription(
       `Creates a clickable button to accept a steam invite from a URL.`
     )
+    // .addStringOption((option) =>
+    //   option
+    //     .setName("invitelink")
+    //     .setDescription('The Steam invite link. Starts with "steam://"')
+    //     .setRequired(true)
+    // )
     .addStringOption((option) =>
       option
-        .setName("invitelink")
-        .setDescription('The Steam invite link. Starts with "steam://"')
+        .setName("profileurl")
+        .setDescription("Link to your steam profile.")
         .setRequired(true)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("neverexpire")
+        .setDescription(
+          "Decide if you want the invite to not expire after a minute."
+        )
+        .setRequired(false)
     ),
   async execute(interaction) {
-    const steamURL = interaction.options.getString("invitelink");
+    // const steamURL = interaction.options.getString("invitelink");
     const steamURLRegex = /^steam:\/\/joinlobby\/\d+\/\d+$/;
-    const isSteamURLValid = steamURLRegex.test(steamURL);
+    const steamIPRegex = /^steam:\/\/connect\/(\d{1,3}\.){3}\d{1,3}:\d+$/;
+    // const isSteamURLValid = steamURLRegex.test(steamURL);
 
-    if (!isSteamURLValid) {
+    // if (!isSteamURLValid) {
+    //   return await interaction.reply(
+    //     "Your steam URL appears to be invalid. Please double check the URL and try again."
+    //   );
+    // }
+
+    const neverExpire = interaction.options.getBoolean("neverexpire");
+    const profileURL = interaction.options.getString("profileurl");
+    const response = await axios.get(
+      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${STEAM_API_KEY}&format=json&steamids=76561198202659223`
+    );
+    if (!response.status == 200) {
       return await interaction.reply(
-        "Your steam URL appears to be invalid. Please double check the URL and try again."
+        "An error ocurred. Please try again later."
       );
     }
+    const data = response.data;
+    const playerData = data.response.players[0];
+    if (!playerData.lobbysteamid || !playerData.gameid) {
+      return await interaction.reply(
+        "Player appears to not be in a lobby currently."
+      );
+    }
+    const steamURL = `steam://joinlobby/${playerData.gameid}/${playerData.lobbysteamid}/${playerData.steamid}`;
 
     const BASE_URL =
       "https://frostemanneogard.github.io/uri-redirector/?uri=" + steamURL;
@@ -50,6 +89,9 @@ module.exports = {
       embeds: [responseEmbed],
       components: [row],
     });
+    if (neverExpire == true) {
+      return;
+    }
     const collectorFilter = (i) => i.user.id === interaction.user.id;
     const timeoutEmbed = new EmbedBuilder()
       .setTitle("Steam Invitation")
@@ -62,7 +104,7 @@ module.exports = {
     try {
       await message.awaitMessageComponent({
         filter: collectorFilter,
-        time: 60_000,
+        time: 120_000,
       });
     } catch (e) {
       await interaction.editReply({ embeds: [timeoutEmbed], components: [] });
