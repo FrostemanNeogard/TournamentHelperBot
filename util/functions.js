16e133eeba981a499310078e51f79102d8fb88ed
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-03 23:16:08 +0200

    create getsteaminvitelinkfromprofileurl function

--- util/functions.js
+++ util/functions.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const { Exception } = require("sass");
 require("dotenv").config();
+const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 const getFiles = (path, ending) => {
   return fs.readdirSync(path).filter((f) => f.endsWith(ending));
@@ -14,6 +16,25 @@ const capitalizeFirstLetters = (string) => {
     output.push(capitalizedWord);
   }
   return output.join(" ");
+};
+
+const getSteamInviteLinkFromProfileURL = async (profileURL) => {
+  const steamId = 76561198202659223;
+  const response = await axios.get(
+    `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${STEAM_API_KEY}&format=json&steamids=${steamId}`
+  );
+
+  if (!response.status == 200) {
+    throw new Exception("An error ocurred. Please try again later.");
+  }
+
+  const playerData = response.data.response.players[0];
+
+  if (!playerData.lobbysteamid || !playerData.gameid) {
+    throw new Exception("Player appears to not be in a lobby currently.");
+  }
+
+  return `steam://joinlobby/${playerData.gameid}/${playerData.lobbysteamid}/${playerData.steamid}`;
 };
 
 module.exports = {
