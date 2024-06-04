b5f2cdd2fbc5b245620aec6304626a97f00daacc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-04 23:43:24 +0200

    extract start api fetching to separate function

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -38,31 +38,57 @@ export function getStartSlugFromStartURL(startURL: string) {
 }
 
 export async function getStartEventIdFromStartSlug(startSlug: string) {
-  const response = await fetch(START_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + process.env.START_API_KEY,
-    },
-    body: JSON.stringify({
-      query: `
+  const query = `
       query getEventId($slug: String) {
         event(slug: $slug) {
           id
           name
         }
-      },`,
-      variables: {
-        slug: startSlug,
-      },
-    }),
-  });
+      },`;
 
-  const data = await response.json();
+  const variables = {
+    slug: startSlug,
+  };
+
+  const data = await fetchStartApi(query, variables);
   return data.data.event.id;
 }
 
 export async function getStartEntrantsFromEventId(eventId: string) {
+  const query = `
+    query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
+      event(id: $eventId) {
+        id
+        name
+        standings(query: {
+          perPage: $perPage,
+          page: $page
+        }){
+          nodes {
+            placement
+            entrant {
+              id
+              name
+            }
+          }
+        }
+      }
+    }`;
+
+  const variables = {
+    eventId: eventId,
+    page: "1",
+    perPage: "8",
+  };
+
+  const data = await fetchStartApi(query, variables);
+  return data.data.event.standings.nodes;
+}
+
+export async function fetchStartApi(
+  query: string,
+  variables: { [key: string]: any }
+) {
   const response = await fetch(START_API_URL, {
     method: "POST",
     headers: {
@@ -70,33 +96,11 @@ export async function getStartEntrantsFromEventId(eventId: string) {
       Authorization: "Bearer " + process.env.START_API_KEY,
     },
     body: JSON.stringify({
-      query: `
-      query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
-        event(id: $eventId) {
-          id
-          name
-          standings(query: {
-            perPage: $perPage,
-            page: $page
-          }){
-            nodes {
-              placement
-              entrant {
-                id
-                name
-              }
-            }
-          }
-        }
-      }`,
-      variables: {
-        eventId: eventId,
-        page: "1",
-        perPage: "8",
-      },
+      query: query,
+      variables: variables,
     }),
   });
 
   const data = await response.json();
-  return data.data.event.standings.nodes;
+  return data;
 }
