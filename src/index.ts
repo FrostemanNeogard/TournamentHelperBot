0b7bef149a82857432ebf990c19d5f592a0462c3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-04 19:59:44 +0200

    fix command fetching

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits } from "discord.js";
 import { readdirSync } from "fs";
+import { readdir } from "fs/promises";
 
 const client = new Client({
   intents: [
@@ -14,18 +15,21 @@ function getFiles(path: string, ending: string) {
   return readdirSync(path).filter((f) => f.endsWith(ending));
 }
 
-readdirSync("./commands/").forEach((category) => {
-  let commands = getFiles(`./commands/${category}`, ".js");
+async function refreshCommands() {
+  const commandCategories = await readdir(`src/commands`);
+  commandCategories.forEach((category) => {
+    let commands = getFiles(`src/commands/${category}`, ".ts");
 
-  commands.forEach((file) => {
-    delete require.cache[require.resolve(`../commands/${category}/${file}`)];
-
-    const command = require(`../commands/${category}/${file}`);
-    client?.application?.commands.set(command.name, command);
+    commands.forEach((file) => {
+      delete require.cache[require.resolve(`./commands/${category}/${file}`)];
+      const command = require(`./commands/${category}/${file}`);
+      client?.application?.commands.set(command.name, command);
+    });
   });
-});
+}
 
 client.on("ready", async (c) => {
+  await refreshCommands();
   console.log(`${client.user?.username} is now online!`);
   console.log(await client.application?.commands.fetch());
 });
