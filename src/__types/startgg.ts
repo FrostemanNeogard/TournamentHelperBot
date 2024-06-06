b68a48cb06be9cce9ba55dfccbd69150b71d1369
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-06 13:57:45 +0200

    add startset type

--- src/__types/startgg.ts
+++ src/__types/startgg.ts
@@ -7,3 +7,16 @@ export type StartPlayer = {
   placement: number;
   entrant: StartEntrant;
 };
+
+export type StartSet = {
+  stream: {
+    enabled: boolean;
+    link: string;
+  };
+  round: number;
+  state: number;
+  players: {
+    name: string;
+    score: number;
+  }[];
+};
