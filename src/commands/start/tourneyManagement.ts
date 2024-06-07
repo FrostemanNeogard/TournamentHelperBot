2290c40cf85bf63acab9ae8e3f62aef7968867bc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-07 10:20:41 +0200

    remove updateset command

--- src/commands/start/tourneyManagement.ts
+++ src/commands/start/tourneyManagement.ts
@@ -11,37 +11,6 @@ export class TourneyManagement {
     /^https:\/\/www\.start\.gg\/tournament\/[^\/]+\/event\/[^\/]+\/?$/;
 
   @Slash({
-    description: "Update the data for a given set by ID.",
-  })
-  async updateset(
-    @SlashOption({
-      description: "Link to a start.gg tournament.",
-      name: "tournamentlink",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    })
-    startURL: string,
-    @SlashOption({
-      description: "ID of the set you want to update.",
-      name: "setid",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    })
-    setId: string,
-    interaction: CommandInteraction
-  ): Promise<void> {
-    const isValidTournamentLink = this.startURLRegex.test(startURL);
-    if (!isValidTournamentLink) {
-      await interaction.reply(
-        "Your start.gg URL appears to be invalid. Please double check the URL and try again."
-      );
-      return;
-    }
-
-    await interaction.reply("This command has not been implemented yet.");
-  }
-
-  @Slash({
     description: "Reset a given set by ID.",
   })
   async resetset(
