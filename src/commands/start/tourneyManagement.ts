34e03b8c9b513a90646fe054cd7bdd41b5305006
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-07 10:01:27 +0200

    create resetset command

--- src/commands/start/tourneyManagement.ts
+++ src/commands/start/tourneyManagement.ts
@@ -1,6 +1,9 @@
 import type { CommandInteraction } from "discord.js";
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
+import { resetSetById } from "../../util/functions";
+import { COLORS } from "../../util/config";
+import { GraphqlError } from "../../__types/startgg";
 
 @Discord()
 export class TourneyManagement {
@@ -36,5 +39,59 @@ export class TourneyManagement {
     }
 
     await interaction.reply("This command has not been implemented yet.");
+  }
+
+  @Slash({
+    description: "Reset a given set by ID.",
+  })
+  async resetset(
+    @SlashOption({
+      description: "Link to a start.gg tournament.",
+      name: "tournamentlink",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    startURL: string,
+    @SlashOption({
+      description: "ID of the set you want to reset.",
+      name: "setid",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    setId: string,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const isValidTournamentLink = this.startURLRegex.test(startURL);
+    if (!isValidTournamentLink) {
+      await interaction.reply(
+        "Your start.gg URL appears to be invalid. Please double check the URL and try again."
+      );
+      return;
+    }
+
+    const responseEmbed = new EmbedBuilder();
+
+    const errors: GraphqlError = await resetSetById(setId);
+    if (errors) {
+      responseEmbed
+        .setColor(COLORS.danger)
+        .setTitle("Reset Set")
+        .setFields({
+          name: "An error occurred",
+          value: errors.errors[0].message ?? "No error message received",
+        });
+      await interaction.reply({ embeds: [responseEmbed] });
+      return;
+    }
+
+    responseEmbed
+      .setColor(COLORS.main)
+      .setTitle("Success")
+      .setFooter({ text: `Set ID: ${setId}` })
+      .setFields({
+        name: "Success!",
+        value: "The set was reset successfully.",
+      });
+    await interaction.reply({ embeds: [responseEmbed] });
   }
 }
