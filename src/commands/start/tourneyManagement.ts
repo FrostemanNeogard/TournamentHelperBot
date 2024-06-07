6838679bf49bc31dfe884db9bc98652df0a4735b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-07 10:20:15 +0200

    create reportset command

--- src/commands/start/tourneyManagement.ts
+++ src/commands/start/tourneyManagement.ts
@@ -1,9 +1,9 @@
 import type { CommandInteraction } from "discord.js";
 import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
-import { resetSetById } from "../../util/functions";
+import { reportSetById, resetSetById } from "../../util/functions";
 import { COLORS } from "../../util/config";
-import { GraphqlError } from "../../__types/startgg";
+import { GraphqlError, StartSetReportData } from "../../__types/startgg";
 
 @Discord()
 export class TourneyManagement {
@@ -76,6 +76,65 @@ export class TourneyManagement {
       .setFields({
         name: "Success!",
         value: "The set was reset successfully.",
+      });
+    await interaction.reply({ embeds: [responseEmbed] });
+  }
+
+  @Slash({
+    description: "Report data for a given set by ID.",
+  })
+  async reportset(
+    @SlashOption({
+      description: "ID of the set you want to report.",
+      name: "setid",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    setId: string,
+    @SlashOption({
+      description: "Player One's new score.",
+      name: "playeronescore",
+      required: true,
+      type: ApplicationCommandOptionType.Number,
+    })
+    playerOneScore: number,
+    @SlashOption({
+      description: "Player Two's new score.",
+      name: "playertwoscore",
+      required: true,
+      type: ApplicationCommandOptionType.Number,
+    })
+    playerTwoScore: number,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const responseEmbed = new EmbedBuilder();
+
+    const gameData: StartSetReportData = {
+      gameNum: 3,
+      newPlayerOneScore: playerOneScore,
+      newPlayerTwoScore: playerTwoScore,
+    };
+
+    const errors: GraphqlError = await reportSetById(setId, gameData);
+    if (errors) {
+      responseEmbed
+        .setColor(COLORS.danger)
+        .setTitle("Reset Set")
+        .setFields({
+          name: "An error occurred",
+          value: errors.errors[0].message ?? "No error message received",
+        });
+      await interaction.reply({ embeds: [responseEmbed] });
+      return;
+    }
+
+    responseEmbed
+      .setColor(COLORS.main)
+      .setTitle("Success")
+      .setFooter({ text: `Set ID: ${setId}` })
+      .setFields({
+        name: "Success!",
+        value: "The set was updated successfully.",
       });
     await interaction.reply({ embeds: [responseEmbed] });
   }
