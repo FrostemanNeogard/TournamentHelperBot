b81f2ad42092705dfda48cf036cd95222e376691
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-07 10:19:22 +0200

    add mutation for reporting set data

--- src/util/graphqlMutations.ts
+++ src/util/graphqlMutations.ts
@@ -4,3 +4,14 @@ export const mutationResetSet = `
       id
     }
   }`;
+
+export const mutationReportSet = `
+  mutation UpdateSetById($setId: ID!, $winnerId: ID, $gameNum: Int!, $entrant1Score: Int, $entrant2Score: Int) {
+    reportBracketSet(setId: $setId, winnerId: $winnerId gameData: {
+      gameNum: $gameNum
+      entrant1Score: $entrant1Score
+      entrant2Score: $entrant2Score
+    }) {
+      fullRoundText
+    }
+  }`;
